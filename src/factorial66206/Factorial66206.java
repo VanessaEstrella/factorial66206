5bd3985f16e60dee5d7d6840f23e54643825007f
starco <starco@192.168.10.217>
2021-02-25 21:40:30 -0600

    mi primer commit

---
+++ src/factorial66206/Factorial66206.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorial66206;
+
+/**
+ *
+ * @author starco
+ */
+public class Factorial66206 {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero = 4;
+        int factorial = 1;
+        while (numero!=0);{
+        factorial = factorial*numero;
+        numero--;
+    }
+        System.out.println(factorial);
+        
+        
+    }
+    
+    
+}
+        
+    
+      
+        
+    
+   
